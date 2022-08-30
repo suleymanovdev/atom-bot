0715ec2cd32c70e93f4edd0b40d5a5b72782a9dc
Elvin Suleymanov <97979588+suleymanovdev@users.noreply.github.com>
2022-08-30 17:46:27 +0000

    2.1 Fridrich

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
 # Libs
 from typing import Optional
 import discord
-import datetime, asyncio, random
+import datetime, asyncio, random, time
 from pathlib import Path
 import json
 import contextlib
 import io
 import textwrap
 import os
-import calendar
 import logging
-
 
 from datetime import datetime
 from discord import Member
@@ -20,15 +18,9 @@ from discord.ext.commands import has_permissions, MissingPermissions
 from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
+from colorama import Fore, Back, Style
 
-#Setup
-def clear():
-    os.system("clear")
-
-def get_prefix(bot, message):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-    return prefixes[str(message.guild.id)]
+#################### STATUS ######################################################################################################
 
 bot = commands.Bot(command_prefix=get_prefix, intents=discord.Intents.all(), case_insensitive=True)
 logging.basicConfig(
@@ -37,15 +29,19 @@ logging.basicConfig(
     filename="log.txt"
 )                                               
 
-import time
 def art():
     logo_list = ["	Atom {} Developers",
-                "		    Discord Bot   ",
-                "		    Python Code   "]
+                 "     Discord Bot    ",
+                 "	 by suleymanovdev "]
     for i in logo_list:
         print(f"{i}")
         time.sleep(0.2)
-art()
+
+def get_prefix(bot, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    return prefixes[str(message.guild.id)]
+
 cwd = Path(__file__).parents[0]
 cwd = str(cwd)
 time_now = datetime.now()
@@ -53,22 +49,20 @@ print(f"Bot Path: {cwd} .")
 print(f"Start time: {time_now} .")
 print(f'Ping in {bot.latency} ms.')
 
-
 #Setup for bot run && bot prefix && owm settings
-version = '1.0 Dyric'
 secret_file = json.load(open(cwd + '/secrets.json'))
 bot.config_token = secret_file['token']
 bot.remove_command('help')
-owm_key = '612ad9194bbdf063190cdfe5813f246c'
+owm_key = secret_file['owm_key']
 owm = OWM(owm_key)
 mgr = owm.weather_manager()
+version = secret_file['version']
 
+#################### STATUS ######################################################################################################
 
-#Setup for bot status
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name=f";help | {version}"))
-
 
 @bot.event
 async def on_guild_join(guild):
@@ -91,7 +85,8 @@ async def on_guild_remove(guild):
     with open('prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
 
-#Errors in chat
+#################### ERROR #######################################################################################################
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
@@ -105,33 +100,25 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.BadArgument):
         await ctx.send(' :name_badge:  `|`  **Wrong argument.**')
 
-
-#Version
-@bot.command()
+#################### COMMANDS ####################################################################################################
+        
+art() # Logo And Start Flag
+        
+@bot.command() # Bot Version
 async def botvers(ctx):
     await ctx.send(f"`The bot version is {version}.`")
 
-
-#Help
 @bot.command()
-async def help(ctx):
+async def help(ctx): # Bot Help Function
     emb = f"""```Attention, this is a bot created for programmers and their communities. The bot is under beta-development. Therefore, it will not work for some time.\n\n
-\t;sds - About Atom server.\n\n
 \t;changeprefix - You can change bot prefix on your server.\n\n
 \t;weather - Show weather information(owm). <;weather London>\n\n
 \t;timer - Bot starting timer for you. <;timer 30>\n\n
 \t;botvers - Show the bot's version. <;botvers>```"""
     await ctx.send(emb)
 
-
-#SDS
 @bot.command()
-async def sds(ctx):
-    await ctx.send("`SD✌️ is a community of programmers created by suleymanovdev#9475 ,where you can share various interesting news from the world of information technology. Share codes, discuss codes of other participants. Become one of the coolest programmers.We are committed to ensuring that everyone learns and starts writing code at a professional level.`")
-
-#Timer
-@bot.command()
-async def timer(ctx, seconds):
+async def timer(ctx, seconds): # Bot Timer Function
     try:
         secondint = int(seconds)
         if secondint > 3600:
@@ -150,9 +137,8 @@ async def timer(ctx, seconds):
     except ValueError:
         await ctx.send("***ValueError:*** **You must enter number!, if it doesn't work write** ```<bot prefix>bug``` **command.**")
 
-#Weather
 @bot.command()
-async def weather(ctx, *, arg):
+async def weather(ctx, *, arg): # Bot Weather Function
     if arg != '':
         city_input = arg
         observation = mgr.weather_at_place(city_input)
@@ -163,58 +149,24 @@ async def weather(ctx, *, arg):
         embed.add_field(name='Teperature:', value=w.temperature('celsius'))
         embed.add_field(name='Cloud Percent:', value=w.clouds)
         embed.add_field(name='Humidity:', value=w.humidity)
-        await ctx.send(embed=embed)
+        await ctx.send(embed=embed) 
 
-#Calendar
+@commands.has_permissions(manage_channels=True)
 @bot.command()
-async def calendar(ctx, *, month_arg):
-    a = 0
-    if month_arg == "January":
-        a = 1
-    elif month_arg == "February":
-        a = 2
-    elif month_arg == "March":
-        a = 3
-    elif month_arg == "April":
-        a = 4
-    elif month_arg == "May":
-        a = 5
-    elif month_arg == "June":
-        a = 6
-    elif month_arg == "July":
-        a = 7
-    elif month_arg == "August":
-        a = 8
-    elif month_arg == "September":
-        a = 9
-    elif month_arg == "October":
-        a = 10
-    elif month_arg == "November":
-        a = 11
-    elif month_arg == "December":
-        a = 12
-    else:
-        a = 1
-
-    await ctx.send(calendar.month(2022, a))   
-
-#Change prefix command
-@bot.command()
-async def changeprefix(ctx, prefix):
+async def changeprefix(ctx, prefix): # Change Bot Prefix Function
     with open('prefixes.json', 'r') as f:
         prefixes = json.load(f)
-
     prefixes[str(ctx.guild.id)] = prefix
-
     with open('prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
 
-#Clear
 @bot.command(pass_context=True, aliases=['clean'])
 @commands.has_permissions(manage_channels=True)
-async def clear(ctx, amount=500):
+async def clear(ctx, amount=500): # Chat Clear Function
     await ctx.channel.purge(limit=amount)
 
+#################### FORMS #######################################################################################################
+    
 #Developer Program
 @bot.command()
 async def iamdeveloper(ctx, *, arg):
@@ -261,18 +213,20 @@ async def idea(ctx, *, idea=None):
         embed2 = discord.Embed(title="Sucs!",description=f"Idea was **successfully** submitted to the channel <#808749943942283315>\n**Content:\n{idea}**",color=discord.Color.green())
         await ctx.send(embed=embed2)
 
+##################################################################################################################################
+        
 #About
 @bot.command()
 async def about(ctx):
     members = len(set(bot.get_all_members()))
     emb = discord.Embed(title='Bot Information.',timestamp=ctx.message.created_at, colour=discord.Colour.from_rgb(207, 215, 255))
-    emb.add_field(name='Add to server', value='https://bit.ly/3nKUT8i')
-    emb.add_field(name='Bot website', value='suleymanovdev.github.io/bot (Beta)')
-    emb.add_field(name='Bot server', value='https://discord.gg/5Jx8rX282v')
-    emb.add_field(name='Number of servers', value=f'{len(bot.guilds)}')
-    emb.add_field(name='Number of participants', value=f"{members}")
-    emb.add_field(name='Root', value='suleymanovdev#9475')
-    emb.add_field(name='GitHub', value='https://github.com/suleymanovdev')
+    emb.add_field(name='🔗 ADD ME', value='https://bit.ly/3nKUT8i')
+    emb.add_field(name='🔗 WEBSITE', value='suleymanovdev.github.io/bot (Beta)')
+    emb.add_field(name='🔗 SERVER', value='https://discord.gg/5Jx8rX282v')
+    emb.add_field(name='✌ SERVERS', value=f'{len(bot.guilds)}')
+    emb.add_field(name='✌ PARTICIPANS', value=f"{members}")
+    emb.add_field(name='👑 CREATOR', value='suleymanovdev#9475')
+    emb.add_field(name='🔗 GITHUB', value='https://github.com/suleymanovdev/atom-bot')
     await ctx.send(embed=emb)
 
 #Bot Ping
@@ -291,12 +245,14 @@ async def on_message(message):
                 emb = discord.Embed(title=f'{message.author.display_name}!', description=f'''
      👋 Hello friend, my name is **Atom Bot™**!
 
+     😎 My creator build me for helping you.
+     
      😎 My prefixes are `;`.
      😲 Write the `;help` command to find out all my options!
      📃 Do you want to know a little about me? Write `;about`!
      🤔 Need help with the bot, or found a bug/error? Come to our `Atom Discord Server`!
 
-     🍀 Good luck!
+     👻 Good luck!
     ''')
                 await message.reply(embed=emb)
 
@@ -304,8 +260,7 @@ async def on_message(message):
 @bot.command()
 async def updates(ctx):
     emb = discord.Embed(title='Bot updates.', description='Here my creator writes about my updates.',timestamp=ctx.message.created_at, colour=discord.Colour.from_rgb(207, 215, 255))
-    emb.add_field(
-        name='-')
+    emb.add_field(name='2.1 Fridrich', value='Settings visualization, clear & change prefix permissions.')
     await ctx.send(embed=emb)
 
 #Start
